85144a9383f5c46f99165b4ecee1b6bcb8f5c5b6
HyHyZhaLee <huy.ly2053038@hcmut.edu.vn>
2024-01-14 22:08:41 +0700

    Update app android

--- AndroidUI_AndroidStudio/app/build.gradle.kts
+++ AndroidUI_AndroidStudio/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.androidui_androidstudio"
-        minSdk = 30
+        minSdk = 21
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
